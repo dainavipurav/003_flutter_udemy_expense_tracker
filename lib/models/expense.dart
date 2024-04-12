6e7107e9af9265808cb448dc1b00bb1c238b343d
dainavi.purav <dainavi.purav@cmss.in>
2024-04-12 21:03:17 +0530

    added category enum

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,12 +2,19 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   final String id;
   final String title;
   final DateTime date;
   final double amount;
+  final Category category;
 
-  Expense({required this.title, required this.amount, required this.date})
-      : id = uuid.v4();
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
 }
